86d29ba93287da3fc6e528dcd2801004b5113586
jesseboon04 <70975510+jesseboon04@users.noreply.github.com>
2020-09-21 09:07:31 +0200

    Add files via upload

---
+++ ditbenik.py
@@ -0,0 +1,41 @@
+print("Hello You, ik ben jesse")
+print("Wie ben jij")
+input()
+Jan
+print("Hello Jan")
+
+
+print("ik ben een nieuwkomer op het mediacollege dus om meer over mij beter te leren kennen heb ik een paar vragen bedacht
+print("wat is mijn favoriete sport?")
+      print(" a: basketbal")
+      print(" b: voetbal")
+      print(" c: honkbal")
+      if antwoord == "a" or antwoord == "a'
+      print("wrong") 
+      if antwoord == "b" or antwoord == "b"
+      print("good!!")
+      if antwoord == "c" or antwoord == "c'
+      print("wrong")
+
+print("uit welke stad kom ik?")
+      print(" a: leiden
+      print(" b: Alkmaar")
+      print(" c: Amsterdam")
+      if antwoord == "a" or antwoord == "a"
+            print("Wrong`")
+      if antwoord == "b" or antwoord =="b"
+            print "good"
+      if antwoord == "c" or antwoord == "c"
+            print("wrong")
+
+print(" wat verwacht ik van de opleiding?")
+            print(" a: leuk")
+            print(" b: niet leuk")
+            print(" c ik zie wel wat er gebeurt")
+            if antwoord == "a" or antwoord ==  "a"
+            print("good")
+            if antwoord == "b" or antwoord == "b"
+            print('wrong")
+            if antwoord == "c" or antwoord == "c"
+            print("good") 
+            
