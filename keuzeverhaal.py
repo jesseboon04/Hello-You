d00cc983af62068b6dead7c0cf12d60f8c002be8
jesseboon04 <70975510+jesseboon04@users.noreply.github.com>
2020-10-29 16:18:59 +0100

    Add files via upload

---
+++ keuzeverhaal.py
@@ -0,0 +1,363 @@
+print("Het verhaal van Aladin uit Marokko. Hij is een vluchteling en woont nu in Nederland")
+print("Kies bij elke vraag uit A/a, B/b of C/c")
+#vraag 1
+print("Vraag 1") 
+print("Aladin is gevlucht omdat hij twee gezichten had in Marokko de andere reden is?")
+print("Wat is de reden waarom Aladin wilde gevlucht?")
+print("A : hij was niet meer veilig in Marokko en als hij bij zijn familie bleef bracht hij hun ook in gevaar")
+print("B : er was oorlog")
+print("C : de regering wilde hem oppakken") 
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("dat is goed")
+if antwoord == "B" or antwoord == "b": 
+    print("dat is fout hij is gevlucht omdat hij niet meer veilig was")
+if antwoord == "C" or antwoord == "c":
+    print("FOUT hij was niet meer veilig")
+
+    
+#vraag 2
+# met 1 einde
+print("Vraag 2") 
+print("Met wat voor vervoersmiddel is hij naar Nederland gekomen?")
+print(" A : de boot")
+print(" B : het vliegtuig")
+print(" C : de trein")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("NU IS HIJ DOOD WANT DE BOOT IS GEZONKEN")
+    print(" het goede antwoord is het vliegtuig")
+if antwoord == "B" or antwoord == "b":
+    print(" dit is goed hij is binnen 5 uur in Nederland geland")
+if antwoord == "C" or antwoord == "c":
+    print(" er gaat geen trein naar nederland van uit Marokko")
+
+
+#vraag 3
+print("Vraag 3") 
+print("Waar kwam Aladin terecht na dat geland was?")
+print("A : in een kraakpand")
+print("B : in een hotel")
+print("C : in een azielzoekerscentra")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Nee hij kwam in een asielzoekrscentra")
+if antwoord == "B" or antwoord == "b":
+    print("Nee hij kwam in een asielzoekerscentra")
+if antwoord == "C" or antwoord == "c":
+    print("ja dat klopt")
+  
+
+
+#vraag 4
+print("Vraag 4")
+print("Wat ging Aladin doen toen hij in het asielzoekerscentra terecht kwam?")
+print("A : hij gaat de taal leren")
+print("B : hij doet niks")
+print("C : is de hele dag aan het game")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("dit is goed")
+if antwoord == "B" or antwoord == "b":
+    print("dit is fout hij is de taal aan het leren")
+if antwoord == "C" or antwoord == "c":
+    print("dit is fout hij is de taal aan het leren")
+
+
+#vraag 5
+print("Vraag 5")
+print("Wat is de reden waarom hij de taal wil leren?")
+print("A : hij wil zo snel mogelijk aan het werk")
+print("B : hij wil mensen helpen die nog geen Nederlands kunnen")
+print("C : hij wil de taal leren omdat hij al vier talen kan")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("dit is goed")
+if antwoord == "B" or antwoord == "b":
+    print("FOUT, hij wil zo snel mogelijk aan het werk")
+if antwoord == "C" or antwoord == "c":
+    print("FOUT, hij wil zo snel mogelijk aan het werk")
+
+
+#vraag 6
+print("Vraag 6")
+print("Toen Aladin werkt zocht kreeg hij last van ....?(maak de zin af)")
+print("A : lichamelijke klachten")
+print("B : discrimminatie")
+print("C : geld problemen")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("NEE hij kreeg last van discriminatie")
+if antwoord == "B" or antwoord == "b":
+    print("JA hij heeft heel lang last gehad van discrimminatie")
+if antwoord == "C" or antwoord == "c":
+    print("NEE hij kreeg last van discrimminatie")
+
+
+#vraag 7
+print("Vraag 7") 
+print("Waardoor had hij last van discriminatie?")
+print("A : uiterlijk, kledingstijl en gedrag")
+print("B : zijn adres van het asielzoekerscentra, Marokkaanse identiteit en asiel-identiteitskaart")
+print("C : Marokkaanse identiteit, gedrag en kon niet goed Nederlands")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+      print("NEE hij had bij discriminstie last van zijn adres van het asielzoekerscentra, marokkaanse identiteit en asielzoekers-identiteit")
+if antwoord == "B" or antwoord == "b":
+      print("dit is goed dit waren de drie oorzaken van discrimminatie")
+if antwoord == "C" or antwoord == "c":
+      print("NEE hij had bij discriminstie last van zijn adres van het asielzoekerscentra, marokkaanse identiteit en asielzoekers-identiteit")
+
+#vraag 8
+print("Aladin werd een keer aangenomen als boekhouder maar door .... werd hij opeens niet geschikt voor het beroep.")
+print("waardoor werd was hij opeens niet geschikt voor het beroep?")
+print(" A : toen de mensen zijn asiels-identiteit zagen")
+print(" B : toen de mensen zijn adres zagen")
+print(" C : toen de mensen zijn Marokkaanse identitteit zagen")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print(" JA dat is goed")
+if antwoord == "B" or antwoord == "b":
+    print("FOUT toen ze zijn asiels-identiteit zagen")
+if antwoord == "C" or antwoord == "c":
+    print("nee toen ze zijn asielzoekrs-identiteit zagen")
+
+#vraag 9
+print("Volgens Aladin krijgen de vluchtelingen een stempel. wat voor stempel krijgen ze?")
+print(" A : de stempel dat ze niet goed kunnen werken")
+print(" B : de stempel dat minder vriendelijk zijn")
+print(" C : de stempel dat ze Nederlanders willen bestelen")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("NEE de stempel dat ze Nederlanders willen bestelen")
+if antwoord == "B" or antwoord == "b":
+    print("Nee de stempel dat ze Nederlanders willen bestelen")
+if antwoord == "C" or antwoord == "c":
+    print("Dit klopt")
+
+#vraag 10
+print("Aladin kwam in een vicuele crikel waardoor hij moeilijk kon vinden nu is hij uit de cirkel door ......die hem geholpen hebben")
+print(" A : vluchtelingen werk")
+print(" B : uitzendbureau")
+print(" C : vrienden")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("JA vluchtelinen werk heeft hem geholpen")
+if antwoord == "B" or antwoord == "b":
+    print("NEE vluchtelingen werk heeft hem geholpen")
+if antwoord == "C" or antwoord == "c":
+    print("NEE vluchtelingen werk heeft hem geholpen")
+print("--------------------------------------------------------------------------------") 
+
+
+
+
+
+print("NIEUWVERHAAL")
+print("Mohammed woont in Syrie waar een burgeroorlog is")
+print("Mohammed is 16 jaar oud")
+print("Mohammed moet 3 keer per week naar school maar dat is heel gevaarlijk. Er staan veel uitkijk posten van de regering maar er kan elk moment een gevcht onstaan")
+print("want er is een terrositische groep die koerden heet, de koerden zijn tegen de regering")
+
+#vraag 11
+#met einde 
+print("vraag 1")
+print("Mohammed loopt richting school langs een uitkijk post van de regering. Plots komen er allemaal auto's aan met allemaal mannen achterop in de pickup met allemaal wappens in hun hand") 
+print("Mohammed schrikt wat moet hij doen om er levend uit te komen?")
+print("A : blijven staan")
+print("B : wegrennen")
+print("C : verstoppen achter een muurtje")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("NU IS HIJ DOOD")
+if antwoord == "B" or antwoord == "b":
+    print("Dit is goed hij rent naar binnen bij een flatje waar hij veilig is")
+if antwoord == "C" or antwoord == "c":
+    print("NU IS HIJ DOOD, want het muurtje stond midden in het vuur gevecht") 
+
+#vraag 12
+print("vraag 2")
+print("Mohammed ziet een uitgang. De uitgang is aan de andere kant van het vuurgevecht dus daar is het veilig")
+print("Mohammed loopt nu weer rustig naar school")
+print("Mohammed zit midden in de les en er gaat een lucht alarm af")
+print("Wat doet gaat Mohammed doen")
+print("A : HIj gaat met zijn klas de schuilkelder in")
+print("B : Hij blijft zitten in de hoop dat de bom niet op zijn school valt")
+print("C : Hij gaat onder zijn tafel zitten")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Dit is goed")
+if antwoord == "B" or antwoord == "b":
+    print("Dit is fout hij gaat naar de schuilkelder")
+if antwoord == "C" or antwoord == "c":
+    print("Dit is fout hij gaat naar de schuilkelder")
+
+#vraag 13
+print("Het was een vals alarm dus er is niks gebeurd")
+print("Mohammed gaat nu met zijn klas de kelder uit en gaan door met de les")
+print("Mohammed heeft tot 4 uur school en loopt nu naar hij huis. Na het avond eten met zijn ouders horen ze een grote knal en heel veel schoten")
+print("Vraag 3")
+print("Wat doet mohammed")
+print("A : hij gaat naar buiten zoeken voor overlevende")
+print("B : hij blijft thuis")
+print("C : hij gaat buiten voetballen")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("dit is niet goed, blijft thuis")
+if antwoord == "B" or antwoord == "b":
+    print("Dit is goed")
+if antwoord == "C" or antwoord == "c":
+    print("Dit is niet goed hij blijft thuis")
+
+#vraag 14
+#met einde 
+print("De volgende ochtend leest Mohammed de krant in de krant ziet hij dat het een aanslag was van de koerden")
+print("nadat hij de krant gelezen had moet Mohammed boodschappen doen van zijn moeder")
+print("op de terug weg van de supermarkt ziet hij dat twee personen van de teroristische groep een huis beroven van de een oude vrouw die in een een rolstoel zit")
+print("Vraag 4")
+print("wat gaat mohammed doen?")
+print("A : Hij gaat een wapen pakken uit de pick-up auto van de koerden en hun dood schieten")
+print("B : Hij rent naar de dichtsbijzeinde post van de regering en zorgt dat hun daar naar toe gaan")
+print("C : Hij rent op hun af en begint zo hard mogelijk schreeuwen zodat de militaire van de regering komen")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Hij schiet 1 persoon neer maar hij wordt zelf tussen zijn ribben geraakt dus NU IS HIJ DOOD. het goede antwoord is dathij rent naar de dichtsbij zijnde post")
+if antwoord == "B" or antwoord == "b":
+    print("Dit is goed en de twee personen worden opgepakt")
+if antwoord == "C" or antwoord == "c":
+    print("Omdat mohammed zoveel aandacht trekt schieten ze hem DOOD. het goede antwoord is dat hij naar de dichtsbij zijnde post rent")
+
+#vraag 15
+print("toen de twee personen op gepakt werden vroegen de militaire van de regering of hij zijn identiteitskaart kon laten zien")
+print("Mohammed liet zijn identiteitskaart zien en daar zagen de militaire dat hij over een paar dagen 17 werd en dus werd opgeroepen voor het leger")
+print("mohammed wilt niet in het leger want zijn vader is een been verloren door de oorlog")
+print("Mohammed gaat met zijn vader praten over dat hij niet het leger in wil")
+print("vraag 5") 
+print("Mohammed heeft een idee. wat is zijn idee?")
+print("A : Mohammed wilt vluchten naar Europa")
+print("B : Mohammed blijft en gaat onderduiken")
+print("C : Hij blijft rustig door leven") 
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+      print("Goed")
+if antwoord == "B" or antwoord == "b":
+      print("Nee hij wilt vluchten naar Europa")
+if antwoord == "C" or antwoord == "c":
+      print("Nee hij wilt vluchten naar Europa")
+
+
+#vraag 16
+print("Mohammed wilt dus vluchten naar Europa omdat hij het leger niet wil maar ook omdat hij klaar is met de oorlog")
+print("Mohammed bespreekt met zijn ouders dat hij naar Europa wil, zijn ouders vinden dat goed en willen hem dan achterna gaan")
+print("vraag 6") 
+print("wat is de makkelijkste route voor Mohammed om europa te bereiken?")
+print(" A : Via Irak dan naar Turkije en dsn met de boot naar Griekenland")
+print("B : Via Syrie naar Turkije en dan naar Griekenland")
+print("C : via Syrie met de boot op de Middellandse zee naar griekenland")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Nee via Syrie  naar Turkije en dan met de boot  is makkelijker")
+if antwoord == "B" or antwoord == "b":
+    print("Ja dit is het makkelijkst en het vieligst")
+if antwoord == "C" or antwoord == "c":
+      print("Nee via Syrie naar Turkije en dan met de boot is het makkelijkst")
+
+
+# vraag 17 
+print("Mohammed wil zo snel mogelijk weg dus hij pakt zijn spullen")
+print("Hij wordt over 2 dagen 17 dus moet hij het land uit zijn")
+print("Vraag 7") 
+print("wat gaat Mohammed doen")
+print("A : Hij wacht en gaat morgen ochtend vroeg weg") 
+print("B : Hij gaat Vannacht weg")
+print("C : Hij gaat over 2 uurtjes de stad uit om naar Turkije te gaan")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Nee dat is te laat en mist hij de trein")
+if antwoord == "B" or antwoord == "b":
+    print("Er is een avondklok dus als de militaire hem zien wordt hij opgepakt en moet hij 3 jaar de gevangenis in")
+if antwoord == "C" or antwoord == "c":
+    print("Ja dit klopt want buiten de stad zijn weinig militaire posten dus wordt hij minder snel gezien. Nadat hij de stad uit is gaat hij met de trein naar de grens van Turkije")
+
+#vraag 18
+print("Mohammed is uit de srad met de trein vlak bij de grens aangekomen")
+print("Mohammed vind het heel erg spanned en moet zorgen dat hij niet gezien wordt")
+print("vraag 8")
+print("Wat is plan om over de grens te gaan zonder gecontroleerd te worden?")
+print(" A: Hij zoekt een gat in het hek en gaat er doorheen")
+print(" B: Hij gaat met meerdere de grens over en zorgt dat hij niet opvalt")
+print(" C: Hij gaat zich in een vrachtwagen verstoppen") 
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Nee dit is te gevaarlijk want de kans is te groot om ogepakt te worden")
+if antwoord == "B" or antwoord == "b":
+    print("Ja dit is zijn plan en het veiligst")
+if antwoord == "C" or antwoord == "c":
+    print("Nee dit is te gevaarlijk want de kans is te groot om ogepakt te worden")
+
+      
+#vraag 19
+print("Mohammed is met de groep in turkije gekomen")
+print("in Turkije zijn vluchtelingen kampen en daar zijn veel ziektes en weinig voedsel en drinken")
+print("Om in Griekenland te komen moet hij langs drie vluchtelingen kampen daar kan hij overnachten")
+print("Bij de derde vluchelingen kamp is het erg druk")
+print("Mohammed is heel erg moei en uitgeput dus wil daar een paar dagen blijven")
+print("Vraag 9") 
+print("is het handig voor mohammed om daar een paar dagen te blijven?")
+print("A : Nee want er zijn veel ziektes")
+print("B : JA want dan heeft hij straks weer meer energie om lange afstanden te lopen")
+print("C : Ja want dan kan hij weer genoeg eten en drinken")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Dit is goed want anders word hij ziek")
+if antwoord == "B" or antwoord == "b":
+    print("FOUT het eten is niet veilig en het drinken is niet zuiver genoeg NU IS HIJ DOOD GEGAAN")
+if antwoord == "C" or antwoord == "c":
+    print("FOUT het eten is niet veilig en het drinken is niet zuiver genoeg NU IS HIJ DOOD GEGAAN")
+
+
+#vraag 20
+print("Hij is hartstikke moe maar hij heeft Griekenland gehaald en gaat door het vast land van griekenland om bij de kust te komen en daar met de boot naar een van de griekse eilanden te komen")
+print("Mohammed heeft de kust bereikt en gaat naar een hotel om daar uit te rusten")
+print("een paar dagen later vol energie gaat hij met de boot naar Corfu. Hij is met een cruiseschip naar Corfu gegaan. Corfu is een Grieks eiland en vanaf dit eiland kan je naar italie met de boot en dat duurt een uurtje.")
+print("Mohammed heeft Italie bereikt. Hij wil hier zijn toekomst uitbereiden")
+print("Vraag 10") 
+print("Wat gaat Mohammed als eerst doen?")
+print("A : een hotel zoeken")
+print("B : een verblijfvergunning aanvragen en de taal leren")
+print("C : Werk zoeken")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("Nee hij ging een verblijfsvergunning aanvragen en de taal leren")
+if antwoord == "B" or antwoord == "b":
+    print("Ja dit ging hij doen want een verblijfvergunning heb je nodig om in Italie te wonen")
+if antwoord == "C" or antwoord == "c":
+    print("Nee hij ging een verblijfsvergunning aanvragen en de taal leren")
+
+#vraag 21
+# met einde 5
+print("Mohammed heeft in de tijd dat hij moest wachten op zijn verblijfsvergunning de taal geleerd")
+print("Vraag 11") 
+print("wat wil Mohammed nu gaan doen?")
+print("A : Hij wil Terug naar zijn landnvan herkomst want hij voelt zich toch niet zo gelukkig in Italie")
+print("B : Hij wil aan het werk zodat zijn ouders naar Italie kunnen komen")
+print("C : Hij wil naar een ander Europees land gaan want hij voelt zich niet gelukkig Italie")
+antwoord = input()
+if antwoord == "A" or antwoord == "a":
+    print("nee HIj wil aan het werk")
+if antwoord == "B" or antwoord == "b":
+    print("ja dit klopt")
+if antwoord == "C" or antwoord == "c":
+    print("Nee hij wilt aan het werk")
+
+
+
+
+
+print("Mohammed woont nu al een paar in Italie en nu heeft hij genoeg geld bij elkaar gespaard om zijn ouders te laten komen naar Italie")
+print("zijn ouders zijn met de auto naar Libanon gegaan en daar met het vliegtuig naar Italie gevlogen")
+print("Mohammed is nu gelukkig in Italie en heeft een mooi huisje en heeft erg naar zijn zin")
+
+
+
+
